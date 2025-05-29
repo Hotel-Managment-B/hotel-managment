20ea05f0c262b15ef6ca6eb12083ff4391b403e8
Bernardo <bernardotobon1@gmail.com>
2025-05-29 10:39:23 -0500

    feat: add ToiletriesSpentList component to display usage history and integrate with ToiletriesSpent

--- src/components/data/ToiletriesSpent.tsx
+++ src/components/data/ToiletriesSpent.tsx
@@ -13,6 +13,7 @@ import {
   increment,
 } from "firebase/firestore";
 import { db } from "../../firebase/Index";
+import ToiletriesSpentList from './ToiletriesSpentList';
 
 const ToiletriesSpent = () => {
   const [selectedOption, setSelectedOption] = useState("");
@@ -22,6 +23,7 @@ const ToiletriesSpent = () => {
   const [filteredOptions, setFilteredOptions] = useState<string[]>([]);
   const [notes, setNotes] = useState("");
   const [rooms, setRooms] = useState<number[]>([]);
+  const [refreshList, setRefreshList] = useState(false);
 
   useEffect(() => {
     const fetchToiletries = async () => {
@@ -87,6 +89,9 @@ const ToiletriesSpent = () => {
       setQuantity(1);
       setSelectedPiece("");
       setNotes("");
+
+      // Activar la actualización de la lista
+      setRefreshList(!refreshList);
     } catch (error) {
       console.error("Error al registrar el uso:", error);
       alert("Hubo un error al registrar el uso.");
@@ -127,101 +132,107 @@ const ToiletriesSpent = () => {
   };
 
   return (
-    <div className="bg-white rounded-3xl border border-blue-400 m-4 shadow-2xl p-8">
-      <h1 className="text-lg font-bold text-blue-900 mb-4 mt-4">
-        Gastos de Artículos de Aseo
-      </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        <div className="mr-0 md:mr-64">
-          <label className="block text-sm font-medium text-blue-900">
-            Fecha
-          </label>
-          <input
-            type="date"
-            className="mt-1 block w-full border rounded-md shadow-sm border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-          />
-        </div>
-        <div className="mb-4 mr-0 md:mr-64">
-          <label
-            htmlFor="optionInput"
-            className="block text-sm font-medium text-blue-900"
-          >
-            Seleccione un artículo de aseo
-          </label>
-          <input
-            id="optionInput"
-            type="text"
-            value={selectedOption}
-            onChange={handleInputChange}
-            className="mt-1 block w-full border rounded-md shadow-sm  border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            list="toiletries-options"
-          />
-          <datalist id="toiletries-options">
-            {filteredOptions.map((option, index) => (
-              <option key={index} value={option} />
-            ))}
-          </datalist>
-        </div>
+    <>
+      <div className="bg-white rounded-3xl border border-blue-400 mt-4 md:mt-16 shadow-2xl p-8">
+        <h1 className="text-lg font-bold text-blue-900 mb-4">
+          Gastos de Artículos de Aseo
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className="mr-0 md:mr-64">
+            <label className="block text-sm font-medium text-blue-900">
+              Fecha
+            </label>
+            <input
+              type="date"
+              className="mt-1 block w-full border rounded-md shadow-sm border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            />
+          </div>
+          <div className="mb-4 mr-0 md:mr-64">
+            <label
+              htmlFor="optionInput"
+              className="block text-sm font-medium text-blue-900"
+            >
+              Seleccione un artículo de aseo
+            </label>
+            <input
+              id="optionInput"
+              type="text"
+              value={selectedOption}
+              onChange={handleInputChange}
+              className="mt-1 block w-full border rounded-md shadow-sm  border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              list="toiletries-options"
+            />
+            <datalist id="toiletries-options">
+              {filteredOptions.map((option, index) => (
+                <option key={index} value={option} />
+              ))}
+            </datalist>
+          </div>
 
-        <div className="mb-4 mr-0 md:mr-64">
-          <label
-            htmlFor="quantityInput"
-            className="block text-sm font-medium text-blue-900"
-          >
-            Cantidad utilizada
-          </label>
-          <input
-            id="quantityInput"
-            type="number"
-            value={quantity}
-            onChange={handleQuantityChange}
-            className="mt-1 block w-full border rounded-md shadow-sm  border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            min="1"
-          />
-        </div>
+          <div className="mb-4 mr-0 md:mr-64">
+            <label
+              htmlFor="quantityInput"
+              className="block text-sm font-medium text-blue-900"
+            >
+              Cantidad utilizada
+            </label>
+            <input
+              id="quantityInput"
+              type="number"
+              value={quantity}
+              onChange={handleQuantityChange}
+              className="mt-1 block w-full border rounded-md shadow-sm  border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              min="1"
+            />
+          </div>
 
-        <div className="mb-4 mr-0 md:mr-64">
-          <label
-            htmlFor="pieceSelect"
-            className="block text-sm font-medium text-blue-900"
-          >
-            Seleccione una Habitación
-          </label>
-          <select
-            id="pieceSelect"
-            value={selectedPiece}
-            onChange={handlePieceChange}
-            className="mt-1 block w-full border rounded-md shadow-sm  border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-          >
-            <option value="">Seleccione...</option>
-            {rooms.map((roomNumber, index) => (
-              <option key={index} value={roomNumber}>{`Habitación ${roomNumber}`}</option>
-            ))}
-          </select>
+          <div className="mb-4 mr-0 md:mr-64">
+            <label
+              htmlFor="pieceSelect"
+              className="block text-sm font-medium text-blue-900"
+            >
+              Seleccione una Habitación
+            </label>
+            <select
+              id="pieceSelect"
+              value={selectedPiece}
+              onChange={handlePieceChange}
+              className="mt-1 block w-full border rounded-md shadow-sm  border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            >
+              <option value="">Seleccione...</option>
+              {rooms.map((roomNumber, index) => (
+                <option key={index} value={roomNumber}>{`Habitación ${roomNumber}`}</option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-blue-900">
+              Observaciones
+            </label>
+            <textarea
+              className="w-full h-16 p-2 border rounded-md shadow-sm  border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              value={notes}
+              onChange={(e) => setNotes(e.target.value)}
+            ></textarea>
+          </div>
+          <div>
+            <button
+              onClick={async () => {
+                await handleRegisterUsage();
+                await handleUpdateQuantity();
+              }}
+              className="mt-4 ml-18 md:ml-32 bg-blue-500 text-white px-4 py-2 rounded-md shadow-sm hover:bg-blue-600"
+            >
+              Registrar Uso
+            </button>
+          </div>
         </div>
-        <div>
-          <label className="block text-sm font-medium text-blue-900">
-            Observaciones
-          </label>
-          <textarea
-            className="w-full h-16 p-2 border rounded-md shadow-sm  border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            value={notes}
-            onChange={(e) => setNotes(e.target.value)}
-          ></textarea>
-        </div>
-        <div>
-          <button
-            onClick={async () => {
-              await handleRegisterUsage();
-              await handleUpdateQuantity();
-            }}
-            className="mt-4 ml-18 md:ml-32 bg-blue-500 text-white px-4 py-2 rounded-md shadow-sm hover:bg-blue-600"
-          >
-            Registrar Uso
-          </button>
-        </div>
+        
       </div>
-    </div>
+      <div className="m-4">
+        <ToiletriesSpentList refresh={refreshList} />
+      </div>
+    </>
   );
 };
 
